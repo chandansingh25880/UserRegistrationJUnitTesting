acc58b914a7ccb3c377648944acad1c432dbd5ba
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-04 16:14:27 +0530

    Added: User need to enter Valid lastName

--- src/main/java/com/bridgelab/userregistration/UserRegistrationDetail.java
+++ src/main/java/com/bridgelab/userregistration/UserRegistrationDetail.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
  */
 public class UserRegistrationDetail {
     private String firstName;
+    private String lastName;
 
     public String getFirstName() {
         Scanner scanner = new Scanner(System.in);
@@ -27,11 +28,31 @@ public class UserRegistrationDetail {
         }
         return matches;
     }
+    public String getLastName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your last name");
+        lastName = scanner.nextLine();
+        return firstName;
+    }
 
     public static void main(String[] args) {
         UserRegistrationDetail UserRegistrationDetail = new UserRegistrationDetail();
         String firstName = UserRegistrationDetail.getFirstName();
         UserRegistrationDetail.validateFirstName(firstName);
+        String lastName = UserRegistrationDetail.getLastName();
+    }
+    /* @Description User enter a valid last name.
+     */
+    public boolean validateLastName(String sing) {
+        Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}+");
+        Matcher matcher = pattern.matcher(lastName);
+        boolean matches = matcher.find();
+        if (matches) {
+            System.out.println("Your Name is valid");
+        } else {
+            System.out.println("Your Name is Invalid");
+        }
+        return matches;
     }
 
 }
